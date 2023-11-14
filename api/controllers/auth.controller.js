82d2e05e2b7a1bf4032ec94669cb099eb0dac0a5
Hsieh <max870701@gmail.com>
2023-11-14 07:57:17 -0800

    Create Sign In API Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -10,6 +12,24 @@ export const signup = async (req, res, next) => {
         res.status(201).json({ message: "User created successfully!" });
     }
     catch (error) {
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(401, 'Wrong Credentials!'));
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const {password: pass, ...restInfo} = validUser._doc;
+        res
+        .cookie('access_token', token, {httpOnly: true, expires: new Date(Date.now() + 24 * 60 * 60)})
+        .status(200)
+        .json(restInfo);
+    } catch (error) {
         next(error);
     }
 };
