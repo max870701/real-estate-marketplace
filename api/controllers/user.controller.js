98f516fb330767860f24d7c8dcc865c78abf9420
Hsieh <max870701@gmail.com>
2023-11-22 23:47:37 -0800

    Add Contact Landlord Functionality to the Listing Page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -36,21 +36,34 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only delete your account!"));
-    try {
-        await User.findByIdAndDelete(req.params.id);
-        res.clearCookie('access_token');
-        res.status(200).json({ message: "User has been deleted." });
-    } catch (error) {
-        next(error);
-    }
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json({ message: "User has been deleted." });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getUserListings = async (req, res, next) => {
-  if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only get your listings!"));
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only get your listings!"));
   try {
     const listings = await Listing.find({ userRef: req.params.id });
     res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return next(errorHandler(404, "User not found!"));
+    const { password: pass, ...restInfo } = user._doc;
+    res.status(200).json(restInfo);
   } catch (error) {
     next(error);
   }
